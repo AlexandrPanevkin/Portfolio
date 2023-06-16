bb19ac4c13eed81972460f30497904df34e9285b
AlexanderPanevkin <panevkin900@gmail.com>
2023-06-16 19:38:11 +0300

    feat: change Footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import sContainer from "../common/styles/Container.module.css";
-import s from './footer.module.css'
+import s from './footer.module.scss'
+import GithubSVG from '../assets/images/Github.svg'
+import GmailSVG from '../assets/images/Gmail.svg'
+import TelegramSVG from '../assets/images/Telegram.svg'
+import LinkedinSVG from '../assets/images/Linkedin.svg'
 
 export const Footer = () => {
+
+    const Github = {
+        backgroundImage: `url(${GithubSVG})`,
+    };
+    const Gmail = {
+        backgroundImage: `url(${GmailSVG})`,
+    };
+    const Telegram = {
+        backgroundImage: `url(${TelegramSVG})`,
+    };
+    const Linkedin = {
+        backgroundImage: `url(${LinkedinSVG})`,
+    };
+
     return (
         <div className={s.footerBox}>
             <div className={`${sContainer.container} ${s.footerContainer}`}>
                 <div className={s.footer}>
-                    <h3>Alexandr Panevkin</h3>
+                    <h3 className={s.text}>Alexandr Panevkin</h3>
                     <div className={s.iconBox}>
-                        <div className={s.icon}></div>
-                        <div className={s.icon}></div>
-                        <div className={s.icon}></div>
-                        <div className={s.icon}></div>
+                        <div style={Github} className={s.icon}></div>
+                        <div style={Gmail} className={s.icon}></div>
+                        <div style={Telegram} className={s.icon}></div>
+                        <div style={Linkedin} className={s.icon}></div>
                     </div>
 
                 </div>
